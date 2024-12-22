cf3a413a02aa732c90fdd79b9f4baa73572ea059
unknown <herofirefox10@gmail.com>
2024-12-22 18:02:28 +0700

    first commit

---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import App from "../pages/Home/index";
+import About from "../pages/About/about";
+import Dashboard from "../pages/Dashboard";
+import Contact from "../pages/Contact/index";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="contact" element={<Contact />} />
+
+        {/* Auth */}
+        {"Auth"}
+        {/* <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route> */}
+
+        {/* not found */}
+        <Route path="*" element={<App />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
