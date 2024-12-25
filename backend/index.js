b7e9046169bf9a840bc5e91a788f62e4178fc154
unknown <herofirefox10@gmail.com>
2024-12-25 18:49:50 +0700

    konfigurasi database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const session = require("express-session");
+const cors = require("cors");
+const db = require("./config/db");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+db();
+
+// Konfigurasi Session
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
