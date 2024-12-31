d1555b6548fab477d36ddaa59f185d6121e80cfc
Maniexie <herofirefox10@gmail.com>
2024-12-31 17:24:42 +0700

    perubahan icons back di auth

--- frontend/src/layouts/authLayout.jsx
+++ frontend/src/layouts/authLayout.jsx
@@ -1,10 +1,14 @@
 import { Link, Outlet } from "react-router";
+import ArrowLeft from "../../public/icons/ArrowLeft";
 
 const authLayout = () => {
   return (
     <div>
-      <Link to="/" className="fixed ml-3 top-3">
-        ~=== Back
+      <Link
+        to="/"
+        className="fixed ml-3 top-3 hover:bg-slate-400 p-1 rounded-lg"
+      >
+        <ArrowLeft />
       </Link>
       <Outlet />
     </div>
