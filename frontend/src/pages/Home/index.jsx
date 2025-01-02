3630ace28b6c9d4f5598d601f09ec86d0197ee07
Maniexie <herofirefox10@gmail.com>
2025-01-02 19:18:41 +0700

    achievment add

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import Navbar from "../../components/Navbar";
 import Hero from "../../components/Hero";
 import Companies from "../../components/Companies";
 import Courses from "../../components/Course";
+import Achievement from "../../components/Achievment";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <Companies />
       <Courses />
+      <Achievement />
     </div>
   );
 };
